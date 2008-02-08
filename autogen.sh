948361b90191ef6f9cfee3481eec42d5146f08dd
Jan Michael Alonzo <jmalonzo@unpluggable.com>
2008-02-09 09:00:01 +1100

    Fix build scripts and library dependency. Remove autogen dependency on GNOME
    	modified:   Makefile.am
    	modified:   autogen.sh
    	modified:   configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,42 +1,55 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#! /bin/sh
 
+# Allow invocation from a separate build directory; in that case, we change
+# to the source directory to run the auto*, then change back before running configure
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="pywebkitgtk"
-ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4"
-REQUIRED_AUTOCONF_VERSION=2.60
-REQUIRED_AUTOMAKE_VERSION=1.9.2
-REQUIRED_MACROS="python.m4"
-
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/autogen.sh) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
+ORIGDIR=`pwd`
+cd $srcdir
 
 DIE=0
 
-gnome_autogen=
-gnome_datadir=
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf installed to compile $PROJECT."
+    echo "Install the appropriate package for your distribution,"
+    echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
+    DIE=1
+}
 
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have automake installed to compile $PROJECT."
+    echo "Install the appropriate package for your distribution,"
+    echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+    DIE=1
+}
 
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
+LIBTOOLIZE=libtoolize
+($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+    LIBTOOLIZE=glibtoolize
+    ($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have libtool installed to compile $PROJECT."
+        echo "Install the appropriate package for your distribution,"
+        echo "or get the source tarball at http://ftp.gnu.org/gnu/libtool/"
+        DIE=1
+    }
+}
+
+if test "$DIE" -eq 1; then
+    exit 1
 fi
 
-GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
+rm -rf $top_srcdir/autom4te.cache
+
+aclocal -I m4 || exit $?
+$LIBTOOLIZE --force || exit $?
+autoheader || exit $?
+automake --foreign --add-missing || exit $?
+autoconf || exit $?
+
+cd $ORIGDIR || exit 1
+
+$srcdir/configure $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
